fd0f53b244334d912c25cfed8a5612e37ea48488
Bikescape <bikescapeapp@gmail.com>
2025-07-20 13:51:33 +0200

    Add files via upload

--- js/supabase.js
+++ js/supabase.js
@@ -1,8 +1,9 @@
-// 1. Reemplaza ↓↓↓ tus propias claves
-const SUPABASE_URL = 'https://rafqhqpailpvdvvhgvqo.supabase.co
-';   
-const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJhZnFocXBhaWxwdmR2dmhndnFvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTMwMDEyMjgsImV4cCI6MjA2ODU3NzIyOH0.LpP9i9KS0FYO2tp2F_yqZ1mL9pkIp3L2qiPQeVuLGIE';
+// 1. Importa Supabase desde CDN
+import { createClient } from 'https://unpkg.com/@supabase/supabase-js@2/dist/supabase.esm.js';
 
-// 2. Cliente listo
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
+// 2. Tus claves reales
+const SUPABASE_URL  = 'https://rafqhqpailpvdvvhgvqo.supabase.co'; // reemplaza por la tuya
+const SUPABASE_KEY  = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJhZnFocXBhaWxwdmR2dmhndnFvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTMwMDEyMjgsImV4cCI6MjA2ODU3NzIyOH0.LpP9i9KS0FYO2tp2F_yqZ1mL9pkIp3L2qiPQeVuLGIE'; // reemplaza por la tuya
+
+// 3. Cliente exportado
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
